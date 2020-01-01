c09fcf1cf28ebbf7de205e9ce3b96f65ee38d912
jhony112 <shipaclick@gmail.com>
2020-01-01 06:33:20 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,139 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <header>
+            <!-- Fixed navbar -->
+            <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark navbar-hide">
+                <a class="navbar-brand" href="#">
+                    <img src="http://beta.peexoo.ai/img/peexoo234.ab7075f3.png" alt="pexoo logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav mr-auto">
+                        <!--<li class="nav-item active">-->
+                        <!--<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>-->
+                        <!--</li>-->
+                        <!--<li class="nav-item">-->
+                        <!--<a class="nav-link" href="#">Link</a>-->
+                        <!--</li>-->
+                        <!--<li class="nav-item">-->
+                        <!--<a class="nav-link disabled" href="#">Disabled</a>-->
+                        <!--</li>-->
+                    </ul>
+
+                </div>
+            </nav>
+        </header>
+        <main role="main">
+            <img src="http://beta.peexoo.ai/img/peexoo234.ab7075f3.png" class="h-logo">
+            <router-view/>
+        </main>
+
     </div>
-    <router-view/>
-  </div>
 </template>
+<script>
+    /* eslint-disable no-undef */
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
+    export default {
+        mounted() {
+            JQuery(window).scroll(function () {
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+                // add/remove class to navbar when scrolling to hide/show
+                var scroll = $(window).scrollTop();
+                if (scroll >= 150) {
+                    $('.navbar').removeClass("navbar-hide");
+                    $('.h-logo').hide();
 
-    &.router-link-exact-active {
-      color: #42b983;
+                } else {
+                    $('.navbar').addClass("navbar-hide");
+                    $('.h-logo').show();
+                }
+
+            });
+        }
     }
-  }
-}
+</script>
+<style scoped>
+    @import "assets/css/util.css";
+    @import "assets/css/style.css";
+    @import "assets/css/slick-theme.css";
+
+    #app {
+        font-family: 'Proxima Nova', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+
+    main {
+
+    }
+
+    .h-logo {
+        width: 8em;
+        position: absolute;
+        z-index: 10000;
+        left: 20px;
+        top: 10px;
+    }
+
+    #nav {
+        padding: 30px;
+        z-index: 100;
+    }
+
+    #nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
+
+    #nav a .router-link-exact-active {
+        color: #42b983;
+    }
+
+    html {
+        position: relative;
+        min-height: 100%;
+    }
+
+    body {
+        /* Margin bottom by footer height */
+        margin-bottom: 60px;
+    }
+
+    .footer {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        /* Set the fixed height of the footer here */
+        height: 60px;
+        line-height: 60px; /* Vertically center the text there */
+        background-color: #f5f5f5;
+    }
+
+    /* Custom page CSS
+    -------------------------------------------------- */
+    /* Not required for template or sticky footer method. */
+
+    body > .container {
+        padding: 60px 15px 0;
+    }
+
+    .footer > .container {
+        padding-right: 15px;
+        padding-left: 15px;
+    }
+
+    code {
+        font-size: 80%;
+    }
+
+    .navbar-brand img {
+
+        width: 120px;
+    }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,281 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container-fluid">
+        <div class="row m-0">
+            <div class="col-12 col-md-7 col-lg-7 m-0 p-0">
+                <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel" data-slide-to="1"></li>
+                        <li data-target="#carousel" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="item-image d-block w-100"
+                                 src="https://source.unsplash.com/1000x800/?photography">
+                            <div class="caption">
+                                <i class="fas fa-map-marker"></i> Benin.Nigeria
+                            </div>
+                        </div>
+                        <div class="carousel-item ">
+                            <img class="item-image d-block w-100"
+                                 src="https://source.unsplash.com/1000x800/?art,wedding">
+                            <div class="caption">
+                                <i class="fas fa-map-marker"></i> Lagos Nigeria
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="item-image d-block w-100"
+                                 src="https://source.unsplash.com/1000x800/?black,youth,happy,american">
+                            <div class="caption">
+                                <i class="fas fa-map-marker"></i> Irvinton. USA
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-12 col-md-5 col-lg-5 info__box">
+                <div class="card">
+
+                    <div class="talk__text">
+                        <span>Live.</span>
+                        <span>Snap.</span>
+                        <span>Remember</span>
+                    </div>
+                    <div class="lora text-left  w-98">
+                        Relive your best memories and highlight meaningful moments through the lens of world-class
+                        professional photographers around you.
+                    </div>
+                    <div class="action__area  flex-sb m-t-50">
+                        <div class="btn btn-primary">
+                            <i class="fas fa-search"></i>
+                            <span class="text-bold text-lg m-l-5">Find a photographer</span>
+                        </div>
+                        <div class="btn btn-outline-primary">
+                            <i class="fas fa-camera"></i>
+                            <span class="text-bold text-lg m-l-5">I am a photographer</span>
+                        </div>
+
+                    </div>
+                    <!--<div class="more">-->
+                    <!--<div class="row">-->
+                    <!--<div class="col-12 col-md-6">-->
+                    <!--<div class="info-box">-->
+                    <!--<div class="icon">-->
+                    <!--<i class="fas fa-search"></i>-->
+                    <!--</div>-->
+                    <!--<div class="flex-col">-->
+                    <!--<div class="">Peexoo Find</div>-->
+                    <!--&lt;!&ndash;<div class="text-muted">&ndash;&gt;-->
+                    <!--&lt;!&ndash;Looking to hire a photographer for a personal shoot or to cover an event? We’ve got the best in the business&ndash;&gt;-->
+                    <!--&lt;!&ndash;</div>&ndash;&gt;-->
+                    <!--</div>-->
+                    <!--</div>-->
+
+
+                    <!--</div>-->
+                    <!--<div class="col-12 col-md-6"></div>-->
+                    <!--</div>-->
+                    <!--</div>-->
+                    <div class="reviews m-t-20 text-left">
+                        <div class="text-dark"> What some of our users think</div>
+                        <div id="carousel2" class="carousel slide review_carousel m-t-20 " data-ride="carousel">
+
+                            <div class="carousel-inner">
+                                <div class="carousel-item active review">
+                                    <img class="user_image"
+                                         src="https://source.unsplash.com/1000x800/?photography">
+                                    <div class="info text-left">
+                                        <div class="text-bold">Alicia Keys</div>
+                                        <star-rating :show-rating="false" :rounded-corners="true" :star-size="10"
+                                                     :increment="0.5" :read-only="true" v-model="rating"></star-rating>
+                                        <div class="text-italics m-t-5 text-sm">Amazing photo quality i cannot imagine
+                                            my wedding pictures any better!!
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 m-t-50">
+                    <h2 class="text-bold text-left">Color Scheme Used</h2>
+                    <img src="../assets/images/yellow-brown-by-schemecolor.png"/>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    /* eslint-disable no-undef */
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    // @ is an alias to /src
+
+    import StarRating from 'vue-star-rating'
+    // import Slick from 'vue-slick';
+
+    export default {
+        name: 'home',
+        components: {
+            StarRating
+        },
+        data() {
+            return {
+                rating: 5,
+            };
+        },
+        mounted() {
+            JQuery('.carousel-fade').carousel({interval: 8000})
+            JQuery('.review_carousel').carousel({interval: 6000})
+        }
+    }
 </script>
+<style lang="scss">
+    .showcase {
+        height: 300px;
+        width: 300px;
+
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+
+    .container-fluid {
+        padding-right: 0 !important;
+        padding-left: 0 !important;
+        background: #f6f6f6 !important;
+    }
+
+    .carousel-fade .carousel-item {
+        height: 100vh;
+        position: relative;
+        border-bottom-right-radius: 80px;
+
+        img {
+            height: 100%;
+            border-radius: inherit;
+            width: 100%;
+            object-fit: cover;
+        }
+        .caption {
+            position: absolute;
+            background: black;
+            left: 20px;
+            color: white;
+            padding: 10px 20px;
+            bottom: 50px;
+            border-radius: 20px;
+        }
+    }
+
+    .card {
+        border: unset !important;
+        min-height: 500px;
+        background: #ffffff !important;
+        padding: 100px 30px 50px 50px;
+        border-top-left-radius: 80px !important;
+        border-bottom-right-radius: 80px !important;
+
+    }
+
+    .info__box {
+        position: absolute;
+        top: 80px;
+        right: 50px;
+        z-index: 100;
+    }
+
+    .talk__text {
+        text-align: left;
+        span {
+            font-weight: 800;
+            font-size: 2.8em;
+            color: #391D0C;
+            letter-spacing: -1px;
+            font-family: Raleway, sans-serif;
+        }
+    }
+
+    .lora {
+        font-family: "Proxima Nova", sans-serif !important;
+        font-size: 1.7em!important;
+        line-height: 1.2em;
+    }
+
+    .btn {
+        padding: 15px 20px !important;
+        /*border-radius: 30px!important;*/
+    }
+
+    .review {
+        display: flex !important;
+        align-items: center;
+        width: 80%;
+        .user_image {
+            width: 4em;
+            height: 4em;
+            border-radius: 50%;
+            object-fit: cover;
+        }
+        .info {
+            margin-left: 5px;
+            line-height: .8em;
+            width: 70%;
+        }
+
+    }
+
+    @media screen and (max-width: 450px) {
+        .info__box {
+            position: relative;
+        }
+
+    }
+
+    @keyframes Gradient {
+        0% {
+            background-position: 0% 50%
+        }
+        50% {
+            background-position: 100% 50%
+        }
+        100% {
+            background-position: 0% 50%
+        }
+    }
+
+    .info-box {
+        display: flex;
+        align-items: center;
+
+        .icon {
+            background: brown;
+            color: white;
+            border-radius: 5px;
+            padding: 10px 15px;
+
+        }
+    }
+
+    .btn-primary {
+        color: #fff;
+        background-color: #863E15 !important;
+        border-color: #863E15 !important;
+    }
+
+    .btn-outline-primary {
+        color: #863E15 !important;
+        border-color: #863E15 !important;
+    }
+</style>
